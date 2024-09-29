b7585548d73d142dcb4d986efabca00f6132d43b
f-noguera <fernando_noguera@icloud.com>
2024-09-28 21:47:44 -0600

    se agrega parámetro para validar que el quiz no supere las 15 preguntas

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 #GUI del usuario
 print('¡Bienvenido(a) a Quizmania!')
 userName = input('¿Cuál es su nombre?\n')
-quizQuestions = input('¿Cuántas preguntas responderá?\n')
+quizLength = int(input(userName + ' ¿Cuántas preguntas responderá?\n')) #agregar una función que permita convertir la primera letra en mayuscula
+
+#condición que valida que quizLength no supere las 15 preguntas
+while True:
+    quizLength = int(input('El quiz no debe superar las 15 preguntas, reintente: '))
+    if quizLength <= 15:
+        break
