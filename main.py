2645ce0367c3285919facc0455db5f47814d7a2d
f-noguera <fernando_noguera@icloud.com>
2024-09-30 19:34:01 -0600

    se agrega todo como prueba

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 # GUI del usuario
 print('¡Bienvenido(a) a Quizmania!')
-userName = input('¿Cuál es su nombre?\n')
+userName = str(input('¿Cuál es su nombre?\n'))
 quizLength = int(input(userName + ' ¿Cuántas preguntas responderá?\n'))
 
 # valida que el quiz no supere las 15 preguntas
 while quizLength > 15:
     quizLength = int(input('¡El quiz no debe exceder las 15 preguntas, reintente!: '))
-
 
 # listado de preguntas para el quiz
 for i in range(1, quizLength + 1):
